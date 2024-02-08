e65ea8c136ae455224f1696c10cfec2976072b3d
alekben <abenditch@gmail.com>
2024-02-08 12:16:02 -0500

    tinyvideo2

--- tinyvideo/tinyvideo.js
+++ tinyvideo/tinyvideo.js
@@ -29,26 +29,26 @@ async function joinChannel() {
     videoTrack = await AgoraRTC.createCameraVideoTrack({encoderConfig: "720p_2"});
     options.uid = await client.join(options.appid, options.channel, null, null);
     videoTrack.play("local");
-    $("#local-id").text(`Local ID: ${options.uid})`);
+    //$("#local_id").text($("#local_id").text().replace(`Local ID: ${options.uid}`));
+    $("#local_id").text(`Local ID: ${options.uid}`);
+
 };
 
 async function handleUserPublished(user, mediaType) {
   if (mediaType === 'video') {
       await client.subscribe(user, mediaType);
       user.videoTrack.play(`remote`);
-      $("#remote-id").text(`Remote ID: ${user.uid})`);
+      $("#remote_id").text(`Local ID: ${user.uid}`);
+      //$("#remote_id").text($("#remote_id").text().replace(`Remote ID: ${user.uid}`));
       subscribe(user, mediaType);
   }
 }
 
 async function handleUserUnpublished(user, mediaType) {
   if (mediaType === 'video') {
-    $(`#remote`).remove();
-    $("#remote-id").text(``);
     videoTrack.stop();
     videoTrack.close();
-    $("#local-name").text(``);
-    client.leave();
-    $("#remote").css("display", "none");
+    await client.leave();
+    $(`#remote`).remove();
   }
 }
