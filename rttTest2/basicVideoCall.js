0a849ddeefcc91888237dbabb6396b5454d86303
alekben <abenditch@gmail.com>
2024-03-11 16:09:58 -0400

    rttTest2 input cleaned

--- rttTest2/basicVideoCall.js
+++ rttTest2/basicVideoCall.js
@@ -1,32 +1,16 @@
-/*
- *  These procedures use Agora Video Call SDK for Web to enable local and remote
- *  users to join and leave a Video Call channel managed by Agora Platform.
- */
 
-/*
- *  Create an {@link https://docs.agora.io/en/Video/API%20Reference/web_ng/interfaces/iagorartcclient.html|AgoraRTCClient} instance.
- *
- * @param {string} mode - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#mode| streaming algorithm} used by Agora SDK.
- * @param  {string} codec - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#codec| client codec} used by the browser.
- */
 var client;
 var rttClient;
-var encryption = false;
 
-/*
- * Clear the video and audio tracks used by `client` on initiation.
- */
 var localTracks = {
   videoTrack: null,
   audioTrack: null
 };
 
-/*
- * On initiation no users are connected.
- */
 var remoteUsers = {};
 var remotesArray = [];
 
+// RTC client for host/audience
 if (!client) {
   client = AgoraRTC.createClient({
     mode: "live",
@@ -34,6 +18,7 @@ if (!client) {
   });
 }
 
+// RTT audience client
 if (!rttClient) {
   rttClient = AgoraRTC.createClient({
     mode: "live",
@@ -203,11 +188,6 @@ function updateUIDs(id, action) {
 }
 }
 
-
-/*
- * When this page is called with parameters in the URL, this procedure
- * attempts to join a Video Call channel using those parameters.
- */
 $(() => {
   initVideoProfiles();
   $(".profile-list").delegate("a", "click", function (e) {
@@ -227,11 +207,7 @@ $(() => {
   }
 });
 
-/*
- * When a user clicks Join or Leave in the HTML form, this procedure gathers the information
- * entered in the form and calls join asynchronously. The UI is updated to match the options entered
- * by the user.
- */
+
 $("#join-form").submit(async function (e) {
   e.preventDefault();
   $("#join").attr("disabled", true);
@@ -272,9 +248,7 @@ $("#join-form").submit(async function (e) {
   }
 });
 
-/*
- * Called when a user clicks Leave in order to exit a channel.
- */
+
 $("#leave").click(function (e) {
   leave();
 });
@@ -296,17 +270,6 @@ $("#subscribe").click(function (e) {
 $("#unsubscribe").click(function (e) {
   manualUnsub();
 });
-$("#setEncryption").click(function (e) {
-  if (!encryption) {
-    setEncryption();
-    encryption = true; 
-    $("#setEncryption").text("Disable Encryption"); 
-  } else {
-    encryption = false;
-    client.setEncryptionConfig("none", "test");
-    $("#setEncryption").text("Enable Encryption"); 
-  }
-});
 
 async function manualSub() {
   //get value of of uid-input
@@ -323,17 +286,13 @@ async function manualUnsub() {
   await client.unsubscribe(user, "");
   $(`#player-wrapper-${id}`).remove();
 }
-/*
- * Join a channel, then create local video and audio tracks and publish them to the channel.
- */
+
 async function join() {
-  // Add an event listener to play remote tracks when remote user publishes.
-  //await setEncryption();
+
   client.on("user-published", handleUserPublished);
   client.on("user-unpublished", handleUserUnpublished);
   rttClient.on("stream-message", handleStreammessage);
 
-  // Join the channel.
   options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
   options.rttUid = await rttClient.join(options.appid, options.channel, options.token || null, null);
 
@@ -348,60 +307,14 @@ async function join() {
     });
   }
 
-
-  // Play the local video track to the local browser and update the UI with the user ID.
   localTracks.videoTrack.play("local-player");
   $("#local-player-name").text(`localVideo(${options.uid})`);
   $("#joined-setup").css("display", "flex");
 
-  // Publish the local video and audio tracks to the channel.
   await client.publish(Object.values(localTracks));
   console.log("publish success");
 }
 
-function hex2ascii(hexx)
-{
-  const hex = hexx.toString();//force conversion
-  let str = '';
-  for (let i = 0; i < hex.length; i += 2)
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  return str;
-}
-
-function getCharCodes(s){
-  let charCodeArr = [];
-  
-  for(let i = 0; i < s.length; i++){
-      let code = s.charCodeAt(i);
-      charCodeArr.push(code);
-  }
-  
-  return charCodeArr;
-}
-
-function base64ToUint8Array(base64Str)
-{
-  const raw = window.atob(base64Str);
-  const result = new Uint8Array(new ArrayBuffer(raw.length));
-  for (let i = 0; i < raw.length; i += 1)
-  {
-    result[i] = raw.charCodeAt(i);
-  }
-  return result;
-}
-
-async function setEncryption() {
-  // Get the channel name and use it as the key
-  //let encryptKey = hex2ascii("GAcORA4HGEFMRUtTRVk=");
-  //let encryptKey = getCharCodes("test");
-  let encryptKey = "test";
-  //let encryptKey = "GAcORA4HGEFMRUtTRVk=";
-  console.log(`secret is ${encryptKey}`);
-  //let encryptSalt = base64ToUint8Array("X5w9T+50kzxVOnkJKiY/lUk82/bES2kATOt3vBuGEDw=");
-  let encryptionMode = "aes-256-gcm";
-  // Start channel encryption, set before every join
-  client.setEncryptionConfig(encryptionMode, encryptKey);
-}
 
 function handleStreammessage(msgUid, data) {
   // use protobuf decode data
@@ -467,9 +380,6 @@ function addTranslateItem(uid, msg) {
 }
 
 
-/*
- * Stop all local and remote tracks then leave the channel.
- */
 async function leave() {
   for (trackName in localTracks) {
     var track = localTracks[trackName];
@@ -480,7 +390,6 @@ async function leave() {
     }
   }
 
-  // Remove remote users and player views.
   remoteUsers = {};
   $("#remote-playerlist").html("");
 
@@ -501,15 +410,8 @@ async function leave() {
   console.log("client leaves channel success");
 }
 
-/*
- * Add the local use to a remote channel.
- *
- * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
- * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
- */
 async function subscribe(user, mediaType) {
   const uid = user.uid;
-  // subscribe to a remote user
   await client.subscribe(user, mediaType);
   console.log("subscribe success");
   if (mediaType === "video") {
@@ -527,12 +429,6 @@ async function subscribe(user, mediaType) {
   }
 }
 
-/*
- * Add a user who has subscribed to the live channel to the local interface.
- *
- * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
- * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
- */
 function handleUserPublished(user, mediaType) {
   const id = user.uid;
   updateUIDs(id, "add");
@@ -540,11 +436,6 @@ function handleUserPublished(user, mediaType) {
   subscribe(user, mediaType);
 }
 
-/*
- * Remove the user specified from the channel in the local interface.
- *
- * @param  {string} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to remove.
- */
 function handleUserUnpublished(user, mediaType) {
   if (mediaType === "video") {
     const id = user.uid;
@@ -555,7 +446,6 @@ function handleUserUnpublished(user, mediaType) {
   }
 }
 
-
 function removeItemOnce(arr, value) {
   var index = arr.indexOf(value);
   if (index > -1) {
@@ -563,4 +453,3 @@ function removeItemOnce(arr, value) {
   }
   return arr;
 }
-
