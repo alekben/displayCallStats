54c5eeb041a771799291965b51c0f66f46f05cd9
Frank Molinaro <56936412+frank005@users.noreply.github.com>
2024-03-15 11:06:48 -0700

    a

--- chatTestFrank/src/index.js
+++ chatTestFrank/src/index.js
@@ -182,7 +182,7 @@ createChatGroupButton.addEventListener("click", () => {
     // Call createGroup to create a chat group. //chatGroupRes = res;
     conn.createGroup(option).then((res) => {
         console.log(res);
-        logger.appendChild(document.createElement('div')).append(`${groupName} has been  created, grab groupId our of the console`)
+        logger.appendChild(document.createElement('div')).append(`${groupName} has been  created, grab groupId out of the console`)
     }).catch((err) => {
         console.log('create group chat failed', err);
         logger.appendChild(document.createElement('div')).append(`Failed to create group ${groupId}, check console for error`)
@@ -260,7 +260,7 @@ getChatGroupMessageHistoryButton.addEventListener("click", () => {
                 messageType:item.type,
                 from: item.from,
                 to: item.to,
-                contents: item.msg,
+                msg: item.msg,
             }) 
         })
         var odIV = document.createElement("div");
