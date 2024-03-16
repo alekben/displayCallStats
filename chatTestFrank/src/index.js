8ca526b68b77790f5e88b7cf7da58fb3d2feb179
Frank Molinaro <56936412+frank005@users.noreply.github.com>
2024-03-15 18:31:27 -0700

    set 4 different palces in css

--- chatTestFrank/src/index.js
+++ chatTestFrank/src/index.js
@@ -12,6 +12,8 @@ var storage = {
 //get elements:
 
 const logger = document.getElementById("log");
+const logGroups = document.getElementById("groups");
+const logMessages = document.getElementById("messages");
 const loginButton = document.getElementById("login");
 const logoutButton = document.getElementById("logout");
 const sendPeerMessageButton = document.getElementById("send_peer_message");
@@ -27,24 +29,25 @@ const getPublicGroupsButton = document.getElementById("getPublicGroups");
 // Register listening events
 conn.addEventHandler('connection&message', {
     onConnected: () => {
-        logger.appendChild(document.createElement('div')).append("Connect success !")
+        $(logger).prepend(`<div>${Date()}Connect success !`)
     },
     onDisconnected: () => {
-        logger.appendChild(document.createElement('div')).append("Logout success !")
+        $(logger).prepend(`<div>Logout success !`)
     },
     onTextMessage: (message) => {
         console.log(message)
-        logger.appendChild(document.createElement('div')).append(message.from + ": " + message.msg)
+        logMessages.appendChild(document.createElement('div')).innerText = `${message.from}: ${message.msg}`
     },
     onTokenWillExpire: (params) => {
-        logger.appendChild(document.createElement('div')).append("Token is about to expire")
+        $(logger).prepend(`<div>Token is about to expire`)
         refreshToken(username, password)
     },
     onTokenExpired: (params) => {
-        logger.appendChild(document.createElement('div')).append("The token has expired, please login again.")
+        $(logger).prepend(`<div>The token has expired, please login again.`)
     },
     onError: (error) => {
         console.log('on error', error)
+        $(logger).prepend(`<div>Error`)
     }
 })
 
@@ -54,7 +57,7 @@ function refreshToken(username, password) {
         .then((res) => {
             let agoraToken = res.accessToken
             conn.renewToken(agoraToken)
-            logger.appendChild(document.createElement('div')).append("Token has been updated")
+            $(logger).prepend(`<div>Token has been updated`)
         })
 }
 
@@ -110,15 +113,15 @@ document.getElementById("register").onclick = function () {
     password = document.getElementById("password").value.toString()
     postData('https://a41.chat.agora.io/app/chat/user/register', { "userAccount": username, "userPassword": password })
         .then((res) => {
-            logger.appendChild(document.createElement('div')).append(`register user ${username} success`)
+            $(logger).prepend(`<div>register user ${username} success`)
         })
         .catch((res)=> {
-            logger.appendChild(document.createElement('div')).append(`${username} already exists`)
+            $(logger).prepend(`<div>${username} already exists`)
         })
 }
 // login
 loginButton.addEventListener("click", () => {
-    logger.appendChild(document.createElement('div')).append("Logging in...")
+    $(logger).prepend(`<div>Logging in...`)
     username = document.getElementById("userID").value.toString()
     $.when(getTokens()).then(function(){
         conn.open({
@@ -126,10 +129,10 @@ loginButton.addEventListener("click", () => {
             agoraToken: storage.token
         }).then((res) => {
             console.log('logged in');
-            logger.appendChild(document.createElement('div')).append(`Login success`);
+            $(logger).prepend(`<div>Login success`);
         }).catch((er) => {
             console.log('log in failed');
-            logger.appendChild(document.createElement('div')).append(`Login failed`);
+            $(logger).prepend(`<div>Login failed`);
         })
     })      
 });
@@ -152,7 +155,7 @@ loginButton.addEventListener("click", () => {
 // logout
 logoutButton.addEventListener("click", () => {
     conn.close();
-    logger.appendChild(document.createElement('div')).append("logout")
+    $(logger).prepend(`<div>logout`)
 });
 
 const groupName = document.getElementById("chat_group_name").value.toString();
@@ -177,16 +180,16 @@ createChatGroupButton.addEventListener("click", () => {
     conn.createGroup(option)
         .then((res) => {
             console.log(res);
-            logger.appendChild(document.createElement('div')).innerText = `${groupName} has been created, groupId has been auto populated`;
+            $(logger).prepend(`<div>${groupName} has been created, groupId has been auto populated`);
             var dataObj = res.data.groupid;
             var odIV = document.createElement("div");
             odIV.style.whiteSpace = "pre";
-            logger.appendChild(odIV).innerText = 'groupid for peer is ' + dataObj;
+            $(logger).prepend(`<div>groupid for peer is ${dataObj}`);
             groupId = dataObj;
         })
         .catch((err) => {
             console.error('Create group chat failed', err);
-            logger.appendChild(document.createElement('div')).innerText = `Failed to create group ${groupId}, check console for error`;
+            $(logger).prepend(`<div>Failed to create group ${groupId}, check console for error`);
         });
 });
 
@@ -197,6 +200,8 @@ destroyChatGroupButton.addEventListener("click", () => {
         destroyChatGroup(groupId)
         groupId = null;
     }
+    destroyChatGroup(groupId)
+    groupId = null;
 });
 
 function destroyChatGroup(groupId) {
@@ -207,14 +212,14 @@ function destroyChatGroup(groupId) {
     conn.destroyGroup(option)
         .then((res) => {
             console.log(res);
-            logger.appendChild(document.createElement('div')).innerText = `${groupId} has been destroyed`;
+            $(logger).prepend(`<div>${groupId} has been destroyed`);
             groupId = null;
             $("#groupId").val("");
             console.log("Clearing groupId text box");
         })
         .catch((err) => {
             console.error('Destroy group chat failed', err);
-            logger.appendChild(document.createElement('div')).innerText = `Failed to destroy group ${groupId}, check console for error`;
+            $(logger).prepend(`<div>Failed to destroy group ${groupId}, check console for error`);
         });
     }
 
@@ -232,35 +237,33 @@ joinChatGroupButton.addEventListener("click", () => {
     };
     conn.joinGroup(options).then((res) => {
         console.log(res);
-        logger.appendChild(document.createElement('div')).append(joinMess)
+        $(logger).prepend(`<div>${joinMess}`)
     }).catch((err) => {
         console.log('join group chat failed', err),
-        logger.appendChild(document.createElement('div')).append(`Failed to join group ${groupId}, check console for error`)
+        $(logger).prepend(`<div>Failed to join group ${groupId}, check console for error`);
     })
 })
 
 // leave group
-joinChatGroupButton.addEventListener("click", () => {
-    // Call joinGroup to send a join request to a chat group.
+leaveChatGroupButton.addEventListener("click", () => {
+    // Call memberAbsence to leave a chat group.
     if (!groupId) {
         groupId = document.getElementById("groupId").value.toString();
     }
-    console.log("Join group " + groupId);
-    var joinMess = username + " has joined the group " + groupId;
-    let options = {
-        groupId: groupId,
-        message: joinMess
+    let option = {
+        groupId: groupId
     };
-    conn.joinGroup(options)
+    conn.leaveGroup(option)
         .then((res) => {
             console.log(res);
-            logger.appendChild(document.createElement('div')).innerText = joinMess;
+            $(logger).prepend(`<div>${username} has left the group ${groupId}`);
         })
         .catch((err) => {
-            console.error('Join group chat failed', err);
-            logger.appendChild(document.createElement('div')).innerText = `Failed to join group ${groupId}, check console for error`;
+            console.error('Leave group chat failed', err);
+            $(logger).prepend(`<div>Failed to leave group ${groupId}. Check console for error.`);
         });
 });
+
 
 // get group chat history
 getChatGroupMessageHistoryButton.addEventListener("click", () => {
@@ -268,12 +271,12 @@ getChatGroupMessageHistoryButton.addEventListener("click", () => {
         groupId = document.getElementById("groupId").value.toString();
     }
 
-    logger.appendChild(document.createElement('div')).innerText = "...getChatGroupMessageHistory...";
+    $(logger).prepend(`<div>...getChatGroupMessageHistory...`);
 
     conn.getHistoryMessages({ targetId: groupId, chatType: "groupChat", pageSize: 20 })
         .then((res) => {
             console.log('getChatGroupMessageHistory success');
-            logger.appendChild(document.createElement('div')).innerText = "getChatGroupMessageHistory success";
+            $(logger).prepend(`<div>getChatGroupMessageHistory success`);
 
             let str = '';
             res.messages.forEach((item) => {
@@ -289,11 +292,11 @@ getChatGroupMessageHistoryButton.addEventListener("click", () => {
 
             var odIV = document.createElement("div");
             odIV.style.whiteSpace = "pre";
-            logger.appendChild(odIV).innerText = 'Message History:' + str;
+            logMessages.appendChild(document.createElement('div')).innerText = `Message History: ${str}`;
         })
         .catch((err) => {
             console.error('getChatGroupMessageHistory failed', err);
-            logger.appendChild(document.createElement('div')).innerText = "getChatGroupMessageHistory failed";
+            $(logger).prepend(`<div>getChatGroupMessageHistory failed`);
         });
 });
 
@@ -313,7 +316,7 @@ sendPeerMessageButton.onclick = function () {
     conn.send(msg)
         .then((res) => {
             console.log('Send private text success');
-            logger.appendChild(document.createElement('div')).innerText = `Message sent to: ${peerId}\nMessage: ${peerMessage}`;
+            $(logger).prepend(`<div>Message sent to: ${peerId}\nMessage: ${peerMessage}`);
         })
         .catch((err) => {
             console.error('Send private text failed', err);
@@ -339,25 +342,22 @@ sendChatGroupMessageButton.addEventListener("click", () => {
         .send(msg)
         .then((res) => {
             console.log('Group chat text successfully');
-            logger
-                .appendChild(document.createElement('div'))
-                .innerText = `Message sent to group: ${groupId}\n${username}: ${groupChatMessage}`
+            $(logger).prepend(`<div>${username} sent message to group ${groupId}`);
+            logMessages.appendChild(document.createElement('div')).innerText = `${username}: ${groupChatMessage}`;
         }).catch((err) => {
             console.log('Failed to send group chat text', err),
-            logger
-                .appendChild(document.createElement('div'))
-                .innerText = `Failed to send message, GroupId empty =>${groupId}, Check console for full error`
+            $(logger).prepend(`<div>Failed to send message, GroupId empty =>${groupId}, Check console for full error`);
         })
 });
 
 // get public groups
 getPublicGroupsButton.addEventListener("click", () => {
-    logger.appendChild(document.createElement('div')).innerText = "...getPublicGroups...";
+    $(logger).prepend(`<div>getting PublicGroups...`);
 
     conn.getPublicGroups({limit: 200, cursor: null})
     .then((res) => {
             console.log('getPublicGroups success');
-            logger.appendChild(document.createElement('div')).innerText = "getPublicGroups success";
+            $(logger).prepend(`<div>getPublicGroups success`);
 
             let str = '';
             var ctr = 0;
@@ -371,10 +371,10 @@ getPublicGroupsButton.addEventListener("click", () => {
             console.log("Counter - " + ctr)
             var odIV = document.createElement("div");
             odIV.style.whiteSpace = "pre";
-            logger.appendChild(odIV).innerText = 'Public group:' + str;
+            logGroups.appendChild(document.createElement('div')).innerText = `Public group: ${str}`;
         })
         .catch((err) => {
             console.error('getPublicGroups failed', err);
-            logger.appendChild(document.createElement('div')).innerText = "getPublicGroups failed";
+            $(logger).prepend(`<div>getPublicGroups failed`);
         });
 });
