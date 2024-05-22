2d242d03b1fec3e032b3cceb3b8a92ebd0b0fccf
alekben <abenditch@gmail.com>
2024-05-22 17:10:12 -0400

    fghjfghj

--- audiotrackconfig/audiotrackconfig.js
+++ audiotrackconfig/audiotrackconfig.js
@@ -185,11 +185,12 @@ AgoraRTC.onMicrophoneChanged = async changedDevice => {
   }
 };
 
+//encoderConfig: curMicProfile.value, 
 async function initDevices() {
   if (localTrackState.published) {
     if (!localTracks.audioTrack) {
       localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
-        encoderConfig: curMicProfile.value, "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
+        "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
       , googHighpassFilter: {exact:audioTrackConfig.googFilter}});
     } else {
       console.log("mic track already exists, replacing.");
@@ -198,7 +199,7 @@ async function initDevices() {
       await localTracks.audioTrack.close();
       localTracks.audioTrack = undefined;
       localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
-        encoderConfig: curMicProfile.value, "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
+        "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
       , googHighpassFilter: {exact:audioTrackConfig.googFilter}});
       publishMic();
       showPopup("Replacing, unmuting, and publishing new mic track")
@@ -211,7 +212,7 @@ async function initDevices() {
       }
     } else {
       localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
-        encoderConfig: curMicProfile.value, "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
+        "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
       , googHighpassFilter: {exact:audioTrackConfig.googFilter}});
       localTrackState.audioTrackEnabled = true;
       localTrackState.audioTrackMuted = false;
@@ -475,7 +476,7 @@ async function toggleWebAudio() {
 async function publishMic() {
   if (!localTracks.audioTrack) {
     localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
-      encoderConfig: curMicProfile.value, "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
+      "AEC": audioTrackConfig.aec, "ANS": audioTrackConfig.ans, "AGC": audioTrackConfig.agc
     , googHighpassFilter: {exact:audioTrackConfig.googFilter}});
   }
     await client.publish(localTracks.audioTrack);
