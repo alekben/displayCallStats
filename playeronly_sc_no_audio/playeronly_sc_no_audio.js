f84086aba38666ca28122f713ea5a60f2a46dcc4
alekben <abenditch@gmail.com>
2024-10-30 19:09:02 -0400

    enable_instant_video back enabled on playeronlyscnoaudio

--- playeronly_sc_no_audio/playeronly_sc_no_audio.js
+++ playeronly_sc_no_audio/playeronly_sc_no_audio.js
@@ -55,7 +55,7 @@ $(() => {
 });
 
 async function joinChannel() {
-  //await AgoraRTC.setParameter("ENABLE_INSTANT_VIDEO", true);
+  await AgoraRTC.setParameter("ENABLE_INSTANT_VIDEO", true);
   try {
     if (!client) {
       client = AgoraRTC.createClient({
