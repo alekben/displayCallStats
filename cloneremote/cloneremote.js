2694ee1e73331df041f1d0339f659c117f31c495
alekben <abenditch@gmail.com>
2024-12-04 17:05:34 -0500

    cloneremote

---
+++ cloneremote/cloneremote.js
@@ -0,0 +1,216 @@
+
+var client;
+AgoraRTC.enableLogUpload();
+
+
+var remoteUsers = {};
+var remotesArray = [];
+var clonedTracks = {};
+
+var options = {
+  appid: null,
+  channel: null,
+  uid: null,
+  token: null
+};
+
+async function changeTargetUID(label) {
+  $(".uid-input").val(`${label}`);
+}
+
+function updateUIDs(id, action) {
+  if (remotesArray.length == 0 && action == "remove") {
+    $(".uid-list").empty();
+    $(".uid-input").val(``);
+  } else {
+  let i = 0;
+  while (i < remotesArray.length) {
+    if (remotesArray[i] == id) {
+      console.log("UID already in list");
+      return;
+    }
+    i++;
+  }
+
+  $(".uid-list").empty();
+  remotesArray.push(id);
+
+  //repopulate
+  let j = 0;
+  while (j < remotesArray.length) {
+    $(".uid-list").append(`<a class="dropdown-item" label="${remotesArray[j]}" href="#">${remotesArray[j]}</a>`);
+    j++;
+  } 
+  $(".uid-input").val(`${remotesArray[0]}`);
+}
+}
+
+
+$(() => {
+  var urlParams = new URL(location.href).searchParams;
+  options.appid = urlParams.get("appid");
+  options.channel = urlParams.get("channel");
+  options.token = urlParams.get("token");
+  options.uid = urlParams.get("uid");
+  if (options.appid && options.channel) {
+    $("#uid").val(options.uid);
+    $("#appid").val(options.appid);
+    $("#token").val(options.token);
+    $("#channel").val(options.channel);
+    $("#join-form").submit();
+  }
+});
+
+$("#join-form").submit(async function (e) {
+  e.preventDefault();
+  $("#join").attr("disabled", true);
+  $("#subscribe").attr("disabled", false);
+  $("#unsubscribe").attr("disabled", false);
+  try {
+    if (!client) {
+      client = AgoraRTC.createClient({
+        mode: "live",
+        codec: "vp8"
+      });
+    }
+    options.channel = $("#channel").val();
+    options.uid = Number($("#uid").val());
+    options.appid = $("#appid").val();
+    options.token = $("#token").val();
+    client.setClientRole("audience");  
+    await join();
+    if (options.token) {
+      $("#success-alert-with-token").css("display", "block");
+    } else {
+      $("#success-alert a").attr("href", `index.html?appid=${options.appid}&channel=${options.channel}&token=${options.token}`);
+      $("#success-alert").css("display", "block");
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    $("#leave").attr("disabled", false);
+  }
+});
+
+$("#leave").click(function (e) {
+  leave();
+});
+$(".uid-list").delegate("a", "click", function (e) {
+  changeTargetUID(this.getAttribute("label"));
+});
+$("#subscribe").click(function (e) {
+  manualSub();
+});
+$("#unsubscribe").click(function (e) {
+  manualUnsub();
+});
+
+
+async function manualSub() {
+  //get value of of uid-input
+  const id = $(".uid-input").val();
+  let user = remoteUsers[id];
+  await subscribe(user, "video");
+  await subscribe(user, "audio");
+}
+
+async function manualUnsub() {
+  //get value of of uid-input
+  const id = $(".uid-input").val();
+  let user = remoteUsers[id];
+  await client.unsubscribe(user, "");
+  $(`#player-wrapper-${id}`).remove();
+}
+
+async function join() {
+  client.on("user-published", handleUserPublished);
+  client.on("user-unpublished", handleUserUnpublished);
+  options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
+}
+
+async function leave() {
+  remoteUsers = {};
+  $("#remote-playerlist").html("");
+
+  remotesArray = [];
+  $(".uid-list").empty();
+  $(".uid-input").val(``);
+
+  clonedTracks = {};
+  // leave the channel
+  await client.leave();
+  $("#join").attr("disabled", false);
+  $("#leave").attr("disabled", true);
+  $("#joined-setup").css("display", "none");
+  $("#subscribe").attr("disabled", true);
+  $("#unsubscribe").attr("disabled", true);
+  console.log("client leaves channel success");
+}
+
+async function subscribe(user, mediaType) {
+  const uid = user.uid;
+  // subscribe to a remote user
+  await client.subscribe(user, mediaType);
+  console.log("subscribe success");
+  if (mediaType === "video") {
+    const player = $(`
+    <div id="player-wrapper-${uid}">
+    <div class="container-controls">
+      <p class="player-name">remoteUser(${uid})</p>
+      <button id="clone-${uid}" type="button" class="btn btn-primary btn-sm">Clone and Play</button>
+    </div> 
+    <div id="player-${uid}" data-size-variant="sm" class="player"></div>
+    <div id="player-${uid}-cloned" data-size-variant="sm" class="player-cloned"></div>
+  </div>
+    `);
+    $("#remote-playerlist").append(player);
+    user.videoTrack.play(`player-${uid}`);
+
+    $(`#clone-${uid}`).click(() => cloneRemote(uid));
+  }
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+}
+
+function handleUserPublished(user, mediaType) {
+  const id = user.uid;
+  updateUIDs(id, "add");
+  remoteUsers[id] = user;
+  subscribe(user, mediaType);
+}
+
+function handleUserUnpublished(user, mediaType) {
+  if (mediaType === "video") {
+    const id = user.uid;
+    removeItemOnce(remotesArray, id);
+    updateUIDs(id, "remove");
+    delete remoteUsers[id];
+    $(`#player-wrapper-${id}`).remove();
+  }
+}
+
+function removeItemOnce(arr, value) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+async function cloneRemote(uid) {
+  if (document.getElementById(`clone-${uid}`).innerHTML == "Stop") {
+    console.log(`clone stopped for ${uid}`);
+    clonedTracks[uid].stop();
+    clonedTracks[uid] = "";
+    $(`#clone-${uid}`).text("Clone and Play");
+  } else {
+    console.log(`clone triggered for ${uid}`);
+    const stream = remoteUsers[uid].videoTrack.getMediaStreamTrack();
+    clonedTracks[uid] = await AgoraRTC.createCustomVideoTrack({"mediaStreamTrack": stream});
+    clonedTracks[uid].play(`player-${uid}-cloned`, {"config": {"fit": "cover"}});
+    $(`#clone-${uid}`).text("Stop");
+  }
+
+}
+
