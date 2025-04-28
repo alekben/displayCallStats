a8d6dadd9f80fc4f8878300ad25f07da030eb654
alekben <abenditch@gmail.com>
2025-04-28 12:18:16 -0400

    sdfgsdfg

--- audiotrackconfig/audiotrackconfig.js
+++ audiotrackconfig/audiotrackconfig.js
@@ -591,7 +591,7 @@ async function subscribe(user, mediaType) {
   }
   if (mediaType === 'audio') {
     user.audioTrack.play();
-    user.audioTrack.play();
+    //user.audioTrack.play();
   }
   showPopup(`Subscribing to ${mediaType} of UID ${uid}`);
 }
